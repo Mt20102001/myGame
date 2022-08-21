3e8d1a6c08fb592eb0b79305531337bb04bea751
Qa160601 <luonyeuvo2001@gmail.com>
2022-08-21 16:38:33 +0700

    first commit

---
+++ Assets/Scripts/Combat_Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Combat_Player : MonoBehaviour
+{
+    #region Public Value
+    public Animator animator;
+    #endregion
+
+    #region Private Value
+    [SerializeField]
+    private GameObject Player;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetTrigger("Attack");
+            Player.GetComponent<Movement_Player>().CanMove = false;
+            Invoke("WaitAttack", 0.8f);
+        }
+    }
+
+    private void WaitAttack()
+    {
+        Player.GetComponent<Movement_Player>().CanMove = true;
+    }
+}
